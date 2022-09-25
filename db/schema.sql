89bd2281cb9b6f94afe0d3a982f9982ecd83ea4f
Averi <averi.sherman@gmail.com>
2022-09-25 18:01:24 -0400

    created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,15 @@ CREATE TABLE department (
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(45) NULL,
+  title VARCHAR(30) NULL,
   salary DECIMAL(10.3) NULL,
   department_id INT NULL,
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
 );
