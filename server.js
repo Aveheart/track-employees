6f9c79e1ff1fa7f2ebae7a097c9f466fff7333a3
Averi <averi.sherman@gmail.com>
2022-09-25 19:58:27 -0400

    set up server js with const require, middleware, connection to database, default response, and applistening

---
+++ server.js
@@ -0,0 +1,32 @@
+const inquirer = require('inquirer')
+const express = require('express');
+const mysql = require('mysql2');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Hello@27',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
+
