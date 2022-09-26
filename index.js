0fba033e11ad98326f6f68aad653fd03d864e0c2
Averi <averi.sherman@gmail.com>
2022-09-25 21:01:46 -0400

    added index.js for functions and prompts;added comments for todos

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const table = require('console.table');
+
+// function to start 
+// function to prompt options: view depts, roles, employees; add dept, role, employee; update employee role
+// switch statement depending on choice for view; view of table
+// input for add 
+// choose update, select which employee, and input to update role and updates database
+// 
--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-const inquirer = require('inquirer')
 const express = require('express');
-const mysql = require('mysql2');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
