0e199a3649c3a7b0f7237d53dc4d7029a075bba5
Averi <averi.sherman@gmail.com>
2022-09-25 21:24:31 -0400

    added menu selection

--- index.js
+++ index.js
@@ -8,3 +8,26 @@ const table = require('console.table');
 // input for add 
 // choose update, select which employee, and input to update role and updates database
 // 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'Hello@27',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+
+function menu () {
+    inquirer.prompt(
+        {
+            type: 'list',
+            name: 'pick',
+            message: 'Select One',
+            choices: ['view departments', 'view roles', 'view employees', 'add a department', 'add an employee', 'update an employee role']
+        }
+
+    )
+
+}
